75ff4f634acc73aea06eb02a4107700c8438877e
IiIymik <sambotemp@gmail.com>
2021-07-20 16:08:33 +0300

    add file

---
+++ src/components/App/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+import {Container} from './App.styled.js'
+
+function App() {
+    return (
+      <Container>
+
+        </Container>
+    )
+}
+
+export default App
---
+++ src/components/App/App.styled.js
@@ -0,0 +1,8 @@
+import css from '@emotion/styled/macro';
+
+export const Container = css.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-gap: 16px;
+  padding-bottom: 24px;
+`;
